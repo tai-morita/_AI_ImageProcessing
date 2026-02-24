33fef8aa7e3aa7fd6bab0ca2fbe4b4bc881b17cf
tai-morita <tai-morita@jmorita-mfg.co.jp>
2026-02-24 10:04:25 +0900

    annotation_to_seed でアノテーションした場所のみ抽出することが可能になった。 ImageJで黒点でアノテーションすればOK

--- test/test_edit_seed.py
+++ test/test_edit_seed.py
@@ -2,13 +2,17 @@
 # ペイントでシード点を編集するわけだが、複数ピクセルにまたがってしまう。
 # 隣接するピクセルは同じシード点になってほしいので、隣接ピクセルは同一ラベルとして統合する。
 
+import re
+import glob
 import os
 import numpy as np
 import tifffile as tiff
 from scipy import ndimage
 from skimage import io
+from skimage import measure
+from skimage.morphology import erosion, ball
 
-def neighbors_lin(coords, arr_shape, neibors=8):
+def neighbors_lin_2d(coords, arr_shape, neibors=8):
     # 8近傍の座標を返す
     # ただし、画像の範囲外の座標は返さない (1ライン増やすのとどっちがのであろう)
     height = coords[0]
@@ -59,14 +63,61 @@ def add_original_seed(seed_path, add_seed_path, frame_number):
     seed_frames[frame_number] = add_seed_frame
     tiff.imwrite(seed_path, seed_frames.astype(np.uint16))
 
-def value_down(input_path):
-# 手動アノテーションのために値を255-> 200に下げる
-    frames = tiff.imread(input_path)
-    frames[frames > 200] = 200
-    dir = os.path.dirname(input_path)
-    tiff.imwrite(os.path.join(dir, "FramesForAnnotation.tif"), frames.astype(np.uint16))
+def delete_isolated(frames, neibors=6):
+    # 隣接６近傍のうち孤立点があれば削除する
+    frames_eroded = np.zeros_like(frames, dtype=bool)
+    frames_eroded[1:, :, :] |= frames[1:, :, :] == frames[:-1, :, :]
+    frames_eroded[:-1, :, :] |= frames[:-1, :, :] == frames[1:, :, :]
+    frames_eroded[:, 1:, :] |= frames[:, 1:, :] == frames[:, :-1, :]
+    frames_eroded[:, :-1, :] |= frames[:, :-1, :] == frames[:, 1:, :]
+    frames_eroded[:, :, 1:] |= frames[:, :, 1:] == frames[:, :, :-1]
+    frames_eroded[:, :, :-1] |= frames[:, :, :-1] == frames[:, :, 1:]
+    
+    frames_isolated = ~frames_eroded
 
+    return frames_isolated
+
+def annotation_to_seed(folder_path, output_path):
+    # フォルダ内のアノテーション画像をまとめてseed画像にする
+    file_paths = glob.glob(os.path.join(folder_path, 'test*.tif'))
+    for file_path in file_paths:
+        basename = os.path.basename(file_path)
+        match = re.search(r'\d+', basename)
+        
+        if "test" in basename:
+            annotation_count = 500
+        else:
+            if match is None:
+                raise ValueError(f"番号を取得できません: {basename}")
+            annotation_count = int(match.group())
+
+            if not (str(annotation_count) in os.path.basename(file_path)):
+                print(f"アノテーション番号 {annotation_count} とファイル名が一致していません: {file_path}")
+        
+        frames = tiff.imread(file_path)
+        
+        if frames.ndim != 3:
+            raise ValueError(f"3Dスタック (Z,Y,X) を想定していますが、shape={frames.shape} です。")
+        
+        frames_binary = (frames > 0).astype(np.uint16)
+        frames_binary_reversed = 65535 - frames_binary * 65535 # 背景とアノテーションを65535にする
+
+        # 背景部分 = 1
+        # 歯列 = 0
+        # アノテーション部分 > 1 で出力されるので、1 以上の部分は統一させる
+        labels = measure.label(frames_binary_reversed, connectivity=2)
+        labels[labels == 1] = 0
+        labels[labels > 1]  = annotation_count
+
+        # 孤立点を削除する
+        labels_isolated = delete_isolated(labels)
+        labels_eroded = labels.copy()
+        labels_eroded[labels_isolated] = 0
+
+        tiff.imwrite(r"./test\Output/test_labels_eroded.tif", labels_eroded.astype(np.uint16))
+        
 if __name__ == "__main__":
+    annotation_to_seed(r"./test\Input\Markers", None)
     """
     edit_seed_3d(
         input_path="./Watershed/Tooth/mask.tif",
@@ -77,4 +128,3 @@ if __name__ == "__main__":
                       r"./test\Input\seed_65.tif",
                       65)
     """
-    value_down(r"./test\Input\data_053.tif")
