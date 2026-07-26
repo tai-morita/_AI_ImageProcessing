ec608b8db7334272ba89fb87b2fe8dd761946e3d
tai-morita <tai-morita@jmorita-mfg.co.jp>
2026-07-27 08:28:46 +0900

    更新

--- Watershed/src/WaterShed_main.py
+++ Watershed/src/WaterShed_main.py
@@ -44,7 +44,10 @@ if __name__ == "__main__":
     # edit_seed_1teeth_per_slice(volume_label_original, volume_label_annotate, seed_path)
     # test_edit_seed_main(input_path, volume_label_annotate)  # これでアノテーションGUIが起動するので、そこで編集して保存する
     output_path = r"D:\_study\ImageProcessing\study\Watershed\Data\Output\test\test.tif"
-    input_path = r"D:\_study\ImageProcessing\study\Watershed\Data\Output\test\edited_volume.tif"
+    volume_label_original = r"D:\_study\ImageProcessing\study\Watershed\Data\Output\test\edited_volume.tif"
+    volume_label_annotate = r"D:\_study\ImageProcessing\study\Watershed\Data\Output\test\edited_volume_annotate.tif"
+    input_path = volume_label_original
     seed_path = r"D:\_study\ImageProcessing\study\Watershed\Data\Output\test\edited_volume_seed.tif"
     output_path = r"D:\_study\ImageProcessing\study\Watershed\Data\Output\test\watershed_edited_volume_conn=26.tif"
+    edit_seed(volume_label_original, volume_label_annotate, seed_path)
     watershed_3d_tiff(input_path, seed_path, output_path, connectivity=connectivity)
